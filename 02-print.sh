2ae8f45b06173776a5cfa538d6d65537c824f702
sai-kor <psai003@gmail.com>
2021-12-10 18:13:07 -0500

    echo command color codes corrected syntax

--- 02-print.sh
+++ 02-print.sh
@@ -2,8 +2,8 @@
 echo hello
 echo Hello World
 echo -e "\e[31mHello\e[0m"
-echo -e "\e[32mHello\e]0m"
-echo -e "\e[33mHello\e]0m"
-echo -e "\e[34mHello\e]0m"
-echo -e "\e[35mHello\e]0m"
-echo -e "\e[36mHello\e]0m"
+echo -e "\e[32mHello\e[0m"
+echo -e "\e[33mHello\e[0m"
+echo -e "\e[34mHello\e[0m"
+echo -e "\e[35mHello\e[0m"
+echo -e "\e[36mHello\e[0m"
