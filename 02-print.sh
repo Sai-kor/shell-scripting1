03d24dee0189ce2227e5eb253e4e5f0008f5683d
sai-kor <psai003@gmail.com>
2021-12-10 18:11:46 -0500

    added colors

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 echo hello
 echo Hello World
+echo -e "\e[31mHello\e[0m"
+echo -e "\e[32mHello\e]0m"
+echo -e "\e[33mHello\e]0m"
+echo -e "\e[34mHello\e]0m"
+echo -e "\e[35mHello\e]0m"
+echo -e "\e[36mHello\e]0m"
