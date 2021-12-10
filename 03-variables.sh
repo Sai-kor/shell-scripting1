6f74b0e3a0059df35d9bee639a7dc63a64009f01
sai-kor <psai003@gmail.com>
2021-12-10 18:38:50 -0500

    corrected echo and date command

--- 03-variables.sh
+++ 03-variables.sh
@@ -2,13 +2,13 @@
 a=100
 echo a=${a}
 b=apple
-echo b={b}
+echo b=${b}
 
 c=true
-echo {c}
+echo ${c}
 
-date=$(date+%F)
-echo today date is ${date}
+Date=$(date+%F)
+echo today date is ${Date}
 
 add=$((2+3))
 echo add=${add}
