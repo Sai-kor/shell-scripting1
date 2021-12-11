8cce3407b2da8fd402348d3c196faa18f43b2157
sai-kor <psai003@gmail.com>
2021-12-11 15:43:11 -0500

    initiate all the components

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -17,10 +17,6 @@
 #Finally restart the service once to effect the changes.
 
 # systemctl restart nginx
-USER_UID=$(id -u)
-if [ "${USER_UID}" -ne 0 ]; then
-echo -e "\e[1;31mYou should be a root user to perform this script\e[0m"
-exit
-fi
+
 yum install nginx -y
 #curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
---
+++ roboshop/roboshop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+USER_UID=$(id -u)
+if [ "${USER_UID}" -ne 0 ]; then
+echo -e "\e[1;31mYou should be a root user to perform this script\e[0m"
+exit
+fi
+
+component=$1
+if [ -z "${component}" ]; then
+  echo -e "\e[1;31mcomponent input missing\e[0m"
+  exit
+fi
+if [ ! -e componenets/"${component}".sh ]; then
+  echo -e "\e[1;31mgiven component script does not exist\e[0m"
+  exit
+fi
+
+bash ${component}.sh
