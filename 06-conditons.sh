4fd1322e2c07cff3ff0f1ed900eaeb5427df2e5f
sai-kor <psai003@gmail.com>
2021-12-11 15:08:45 -0500

    and condition

--- 06-conditons.sh
+++ 06-conditons.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-read -p "Enter your age:" age
+read -r -p "Enter your age:" age
 if [ -z "${age}" ]; then
 echo Input missing
 exit
 fi
-if [ "${age}" -lt 18 ]; then
+if [ ! -z "${age}" -a "${age}" -lt 18 ]; then
   echo "you are minor"
 elif [ "${age}" -gt 60 ]; then
   echo you are a senior citizen
