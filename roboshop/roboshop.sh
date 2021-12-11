fff2990635929d03e47dec4d79af929b8a2b946f
sai-kor <psai003@gmail.com>
2021-12-11 18:18:04 -0500

    corercted roboshop.sh if conition path

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -10,7 +10,7 @@ if [ -z "${component}" ]; then
   echo -e "\e[1;31mcomponent input missing\e[0m"
   exit
 fi
-if [ ! -e componenets/"${component}".sh ]; then
+if [ ! -e components/"${component}".sh ]; then
   echo -e "\e[1;31mgiven component script does not exist\e[0m"
   exit
 fi
