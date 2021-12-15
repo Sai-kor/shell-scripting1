f79a22eeefaa9aa15a27c7b82a2f4cb517405a44
sai-kor <psai003@gmail.com>
2021-12-15 17:25:22 -0500

    redis script update

--- roboshop/Components/databases.sh
+++ roboshop/Components/databases.sh
@@ -19,3 +19,18 @@ DOWNLOAD mongodb
  cd /tmp/mongodb-main
  mongo < catalogue.js &>>${LOG_FILE}&& mongo < users.js&>>${LOG_FILE}
  stat_check $? "Load Schema"
+
+ ##Redis setup
+ curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>{LOG_FILE}
+  stat_check $? "Download Redis Repo"
+
+  yum install redis -y &>>{LOG_FILE}
+  stat_check $? "Install redis"
+
+  sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>{LOG_FILE}
+  stat_check $? "update redis config file"
+ #Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
+
+  systemctl enable redis &>>{LOG_FILE} && systemctl start redis &>>{LOG_FILE}
+  stat_check $? "start redis service"
+
