62a5f3389ccd54aea982665f616e4a9b44477077
sai k <psai0073@gmail.com>
2021-12-17 23:02:38 -0500

    redis conf file fix

--- roboshop/Components/databases.sh
+++ roboshop/Components/databases.sh
@@ -26,14 +26,14 @@ DOWNLOAD mongodb
  curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>{LOG_FILE}
   stat_check $? "Download Redis Repo"
 
-  yum install redis -y &>>{LOG_FILE}
+  yum install redis -y &>>${LOG_FILE}
   stat_check $? "Install redis"
 
-  sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>{LOG_FILE}
+  sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG_FILE}
   stat_check $? "update redis config file"
  #Update the BindIP from 127.0.0.1 to 0.0.0.0 in config file /etc/redis.conf & /etc/redis/redis.conf
 
-  systemctl enable redis &>>{LOG_FILE} && systemctl start redis &>>{LOG_FILE}
+  systemctl enable redis &>>${LOG_FILE} && systemctl start redis &>>${LOG_FILE}
   stat_check $? "start redis service"
 
   ##Rabbitmq setup
