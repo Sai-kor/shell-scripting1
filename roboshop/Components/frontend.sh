c30248cfd7ef137a072bf443d302a1325f061cab
sai k <psai0073@gmail.com>
2021-12-17 22:22:22 -0500

    Update Nginx config file

--- roboshop/Components/frontend.sh
+++ roboshop/Components/frontend.sh
@@ -19,7 +19,15 @@ cd /tmp/frontend-main/static/ && cp -r * /usr/share/nginx/html/
 stat_check $? "copying frontend content"
 
 cp /tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-stat_check $? "Update Nginx config file"
+stat_check $? "copy Nginx config file"
+
+sed -i -e '/catalogue/s/localhost/catalogue.devops.internal/' \
+       -e '/user/s/localhost/user.devops.internal/' \
+       -e '/cart/s/localhost/cart.devops.internal/' \
+       -e '/shipping/s/localhost/shipping.devops.internal/' \
+       -e '/payment/s/localhost/payment.devops.internal/' \
+        /etc/nginx/default.d/roboshop.conf
+        stat_check $? "Update Nginx config file"
 
 systemctl enable nginx &>>${LOG_FILE} && systemctl restart nginx &>>${LOG_FILE}
 stat_check $? "Restart Nginx"
